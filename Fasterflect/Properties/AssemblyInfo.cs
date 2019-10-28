06d8e16a324a93f87a52350f59bdf1820be86f5b
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2019-10-28 06:40:15 +0200

    Fix invalid 0.0.0.0 version

--- Fasterflect/Properties/AssemblyInfo.cs
+++ Fasterflect/Properties/AssemblyInfo.cs
@@ -4,8 +4,8 @@ using System.Runtime.InteropServices;
 
 // Assembly-specific attributes
 
-[assembly: AssemblyDescription("Fast & Simple .NET Reflection Library")]
-[assembly: AssemblyCompany("Buu Nguyen & Morten Mertner")]
+
+
 [assembly: AssemblyProduct("Fasterflect")]
 [assembly: AssemblyCopyright("Copyright © Buu Nguyen & Morten Mertner 2010-2013")]
 [assembly: Guid("ccb4ad24-4aea-47a8-97e5-2b68f44fa6e4")]
@@ -13,3 +13,29 @@ using System.Runtime.InteropServices;
 [assembly: InternalsVisibleTo("FasterflectBenchmark, PublicKey=0024000004800000940000000602000000240000525341310004000001000100b944705daf9bdfb4aa4761fdd835b7f40dbcd8edb40500f652e375701d66c583920e99aba7bb54ac950032d6ecde4d5655493a176ad7585ffd42fda7c4dece93e67019bb3cdb6189a54392b28c3b6d682ac9458300a18f82ea7df2f714c6cb81a287543ea9aaaee0ae1d9b22dcb9c416caa1c487785900bbb0db654fff3080a6")]
 [assembly: InternalsVisibleTo("FasterflectSample, PublicKey=0024000004800000940000000602000000240000525341310004000001000100b944705daf9bdfb4aa4761fdd835b7f40dbcd8edb40500f652e375701d66c583920e99aba7bb54ac950032d6ecde4d5655493a176ad7585ffd42fda7c4dece93e67019bb3cdb6189a54392b28c3b6d682ac9458300a18f82ea7df2f714c6cb81a287543ea9aaaee0ae1d9b22dcb9c416caa1c487785900bbb0db654fff3080a6")]
 [assembly: InternalsVisibleTo("FasterflectTest, PublicKey=0024000004800000940000000602000000240000525341310004000001000100b944705daf9bdfb4aa4761fdd835b7f40dbcd8edb40500f652e375701d66c583920e99aba7bb54ac950032d6ecde4d5655493a176ad7585ffd42fda7c4dece93e67019bb3cdb6189a54392b28c3b6d682ac9458300a18f82ea7df2f714c6cb81a287543ea9aaaee0ae1d9b22dcb9c416caa1c487785900bbb0db654fff3080a6")]
+[assembly: AssemblyTitle("Xpand.Fasterflect")]
+[assembly: AssemblyDescription("Fasterflect netstandard2.0 storngly signed. To debug enable your VS Tools/Options/Debugging/Enable Source Server support")]
+[assembly: AssemblyConfiguration("")]
+
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+
+[assembly: AssemblyVersion("2.0.7.0")]
+[assembly: AssemblyFileVersion("2.0.7.0")]
+//[assembly:AllowPartiallyTrustedCallers]
+//[assembly: SecurityTransparent()]
